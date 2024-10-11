45792e4fd0b9423d9e0e968b07f4cb36fc04f875
Kaltrinë Baliu <93614313+kaltrinabaliu@users.noreply.github.com>
2024-10-11 13:59:15 +0200

    final chnages on project

--- src/components/ContactList.tsx
+++ src/components/ContactList.tsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchAllContacts } from "../redux/contact/contactSlice";
-import AddEditContact from "./AddEditContact"; // Assuming this is the component
+import { fetchAllContacts, deleteContact } from "../redux/contact/contactSlice";
+import AddEditContact from "./AddEditContact";
+import ActionButton from "./ActionButton";
 
 const ContactsList = () => {
+  const [selectedContact, setSelectedContact] = useState(null);
+  const [isAddingOrEditing, setIsAddingOrEditing] = useState(false);
+
   const dispatch = useDispatch();
   const { contacts, loading, error } = useSelector(
     (state: { contact: any }) => state.contact
   );
-
-  const [selectedContact, setSelectedContact] = useState(null);
-  const [isAddingOrEditing, setIsAddingOrEditing] = useState(false);
 
   useEffect(() => {
     dispatch(fetchAllContacts());
@@ -20,26 +21,31 @@ const ContactsList = () => {
   if (error) return <div>{error}</div>;
 
   const handleAddContact = () => {
-    setSelectedContact(null); // Clear selected contact for adding new
-    setIsAddingOrEditing(true); // Show Add/Edit form
+    setSelectedContact(null);
+    setIsAddingOrEditing(true);
   };
 
   const handleEdit = (contact: any) => {
-    setSelectedContact(contact); // Set contact to be edited
-    setIsAddingOrEditing(true); // Show Add/Edit form
+    setSelectedContact(contact);
+    setIsAddingOrEditing(true);
   };
 
   const handleCancel = () => {
-    setIsAddingOrEditing(false); // Go back to contact list
+    setSelectedContact(null);
+    setIsAddingOrEditing(false);
   };
 
   const handleSave = () => {
-    setIsAddingOrEditing(false); // After saving, return to the contact list
-    dispatch(fetchAllContacts()); // Optionally refetch contacts to update the list
+    setIsAddingOrEditing(false);
+    dispatch(fetchAllContacts());
+  };
+
+  const handleDelete = (contactId: number) => {
+    dispatch(deleteContact(contactId));
   };
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto">
       {isAddingOrEditing ? (
         <AddEditContact
           contact={selectedContact}
@@ -48,55 +54,105 @@ const ContactsList = () => {
         />
       ) : (
         <>
-          <div className="flex justify-end mb-4">
-            <button
-              onClick={handleAddContact}
-              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-            >
-              Add Contact
-            </button>
+          <div className="flex justify-between items-conter mb-16">
+            <div className="text-[32px] font-bold">Contacts</div>
+            <ActionButton
+              label="Add Contact"
+              click={() => handleAddContact()}
+              bgColor="bg-blue-500"
+              hoverBgColor="bg-blue-600"
+            />
           </div>
-          <table className="min-w-full bg-white border border-gray-300">
+          <table className="min-w-full bg-white border border-collapse border-gray-300">
             <thead>
-              <tr className="border-b">
-                <th className="text-left py-2 px-4">Name</th>
-                <th className="text-left py-2 px-4">Last Name</th>
-                <th className="text-left py-2 px-4">Address</th>
-                <th className="text-left py-2 px-4">City</th>
-                <th className="text-left py-2 px-4">Country</th>
-                <th className="text-left py-2 px-4">Email</th>
-                <th className="text-left py-2 px-4">Number</th>
-                <th className="py-2 px-4">Edit</th>
-                <th className="py-2 px-4">Delete</th>
+              <tr>
+                <th className="text-center py-2 px-4 border border-slate-300 ">
+                  Name
+                </th>
+                <th className="text-center py-2 px-4 border border-slate-300 ">
+                  Last Name
+                </th>
+                <th className="text-center py-2 px-4 border border-slate-300 ">
+                  Address
+                </th>
+                <th className="text-center py-2 px-4 border border-slate-300 ">
+                  City
+                </th>
+                <th className="text-center py-2 px-4 border border-slate-300 ">
+                  Country
+                </th>
+                <th className="text-center py-2 px-4 border border-slate-300 ">
+                  Emails
+                </th>
+                <th className="text-center py-2 px-4 border border-slate-300 ">
+                  Phone Numbers
+                </th>
+                <th className="text-center py-2 px-4 border border-slate-300 ">
+                  Edit
+                </th>
+                <th className="text-center py-2 px-4 border border-slate-300 ">
+                  Delete
+                </th>
               </tr>
             </thead>
             <tbody>
               {contacts.map((contact: any) => (
                 <tr
                   key={contact.id}
-                  className={`border-b hover:bg-gray-100 ${
-                    contact.id % 2 === 1 ? "bg-gray-200" : "bg-white"
+                  className={`border hover:bg-gray-100 ${
+                    contact.id % 2 === 1 ? "bg-gray-100" : "bg-white"
                   }`}
                 >
-                  <td className="py-2 px-4">{contact.name}</td>
-                  <td className="py-2 px-4">{contact.lastName}</td>
-                  <td className="py-2 px-4">{contact.address}</td>
-                  <td className="py-2 px-4">{contact.city}</td>
-                  <td className="py-2 px-4">{contact.country}</td>
-                  <td className="py-2 px-4">{contact.email}</td>
-                  <td className="py-2 px-4">{contact.phoneNumbers}</td>
-                  <td className="py-2 px-4">
-                    <button
-                      onClick={() => handleEdit(contact)}
-                      className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
-                    >
-                      Edit
-                    </button>
+                  <td className="text-center py-2 px-4 border border-slate-300 ">
+                    {contact.name}
                   </td>
-                  <td className="py-2 px-4">
-                    <button className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600">
-                      Delete
-                    </button>
+                  <td className="text-center py-2 px-4 border border-slate-300 ">
+                    {contact.lastName}
+                  </td>
+                  <td className="text-center py-2 px-4 border border-slate-300 ">
+                    {contact.address}
+                  </td>
+                  <td className="text-center py-2 px-4 border border-slate-300 ">
+                    {contact.city}
+                  </td>
+                  <td className="text-center py-2 px-4 border border-slate-300 ">
+                    {contact.country}
+                  </td>
+                  <td className="text-center py-2 px-4 border border-slate-300 ">
+                    {contact.emails && contact.emails.length > 0 ? (
+                      contact.emails.map((email: string, index: number) => (
+                        <div key={index}>{email}</div>
+                      ))
+                    ) : (
+                      <div>No Email</div>
+                    )}
+                  </td>
+                  <td className="text-center py-2 px-4 border border-slate-300 ">
+                    {contact.phoneNumbers && contact.phoneNumbers.length > 0 ? (
+                      contact.phoneNumbers.map(
+                        (phone: string, index: number) => (
+                          <div key={index}>{phone}</div>
+                        )
+                      )
+                    ) : (
+                      <div>No Phone Number</div>
+                    )}
+                  </td>
+                  <td className="text-center py-2 px-4 border border-slate-300 ">
+                    <ActionButton
+                      label="Edit"
+                      click={() => handleEdit(contact)}
+                      bgColor="bg-green-500"
+                      hoverBgColor="bg-green-600"
+                    />
+                  </td>
+                  <td className="text-center py-2 px-4 border border-slate-300 ">
+                    <ActionButton
+                      label="Delete"
+                      click={() => handleDelete(contact.id)}
+                      bgColor="bg-red-500"
+                      hoverBgColor="bg-red-600"
+                    />
                   </td>
                 </tr>
               ))}
--- src/redux/contact/contactSlice.ts
+++ src/redux/contact/contactSlice.ts
@@ -1,5 +1,4 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { ContactsDto } from "../../entities/ContactsDto";
 import {
   createFetchAllContactsUseCase,
   createCreateContactsUseCase,
@@ -30,12 +29,12 @@ export const fetchAllContacts = createAsyncThunk(
 
 export const createContact = createAsyncThunk(
   "contacts/create",
-  async (contact: ContactDto, {}) => {
+  async (contact: ContactDto, { rejectWithValue }: any) => {
     try {
       const createUseCase = createCreateContactsUseCase();
       return await createUseCase.execute(contact);
     } catch (error) {
-      return rejectWithValue(error);
+      return rejectWithValue(error.message || "Failed to create contact.");
     }
   }
 );
@@ -47,26 +46,31 @@ export const updateContact = createAsyncThunk(
       contactId,
       updatedContact,
     }: { contactId: number; updatedContact: ContactDto },
-    {}
+    { rejectWithValue }: any
   ) => {
     try {
       const updateUseCase = createUpdateContactsUseCase();
-      return await updateUseCase.execute(contactId, updatedContact);
+      const updatedContactResponse = await updateUseCase.execute(
+        contactId,
+        updatedContact
+      );
+
+      return updatedContactResponse;
     } catch (error) {
-      return rejectWithValue(error);
+      return rejectWithValue(error.message || "Failed to update contact.");
     }
   }
 );
 
 export const deleteContact = createAsyncThunk(
   "contacts/delete",
-  async (contactId: string, {}) => {
+  async (contactId: number, { rejectWithValue }: any) => {
     try {
       const deleteUseCase = createDeleteContactsUseCase();
       await deleteUseCase.execute(contactId);
-      return contactId; // Returning contactId for the reducer to remove it from state
+      return contactId;
     } catch (error) {
-      return rejectWithValue(error);
+      return rejectWithValue(error.message || "Failed to delete contact.");
     }
   }
 );
@@ -83,23 +87,11 @@ const contactSlice = createSlice({
       (): any;
       new (): any;
       addCase: {
-        (
-          arg0: any,
-          arg1: (
-            state: { loading: boolean; contacts: any },
-            action: { payload: any }
-          ) => void
-        ): {
+        (arg0: any, arg1: (state: any, action: any) => void): {
           (): any;
           new (): any;
           addCase: {
-            (
-              arg0: any,
-              arg1: (
-                state: { loading: boolean; error: any },
-                action: { error: { message: string } }
-              ) => void
-            ): {
+            (arg0: any, arg1: (state: any, action: any) => void): {
               (): any;
               new (): any;
               addCase: {
@@ -146,7 +138,7 @@ const contactSlice = createSlice({
           action: { payload: any }
         ) => {
           state.loading = false;
-          state.contacts = action.payload; // Update the contacts array
+          state.contacts = action.payload;
         }
       )
       .addCase(
@@ -159,35 +151,38 @@ const contactSlice = createSlice({
           state.error = action.error.message || "Failed to fetch contacts.";
         }
       )
+
       .addCase(
         createContact.fulfilled,
         (state: { contacts: any[] }, action: { payload: any }) => {
-          state.contacts.push(action.payload); // Add new contact to the array
+          state.contacts.push(action.payload);
         }
       )
+
       .addCase(
         updateContact.fulfilled,
         (state: { contacts: any[] }, action: { payload: { id: any } }) => {
-          const index = state.contacts.findIndex(
-            (contact: { id: any }) => contact.id === action.payload.id
-          );
-          if (index !== -1) {
-            state.contacts[index] = action.payload; // Update the contact
+          if (action.payload && action.payload.id) {
+            const index = state.contacts.findIndex(
+              (contact: { id: any }) => contact.id === action.payload.id
+            );
+            if (index !== -1) {
+              state.contacts[index] = action.payload;
+            }
+          } else {
           }
         }
       )
+
       .addCase(
         deleteContact.fulfilled,
         (state: { contacts: any[] }, action: { payload: any }) => {
           state.contacts = state.contacts.filter(
             (contact: { id: any }) => contact.id !== action.payload
-          ); // Remove deleted contact
+          );
         }
       );
   },
 });
 
 export default contactSlice.reducer;
-function rejectWithValue(error: unknown) {
-  throw new Error("Function not implemented.");
-}
